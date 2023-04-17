31c9f967e892087763d48db77931a740de9be704
AFSFDS <TestGit>
2023-04-17 16:38:35 +0800

    新增了分配角色功能

--- service-oa/src/main/java/com/atguigu/auth/mapper/SysRoleMapper.java
+++ service-oa/src/main/java/com/atguigu/auth/mapper/SysRoleMapper.java
@@ -10,9 +10,5 @@ import java.util.Map;
 @Repository
 public interface SysRoleMapper extends BaseMapper<SysRole> {
 
-    //1 查询所有角色 和 当前用户所属角色
-    Map<String, Object> findRoleDataByUserId(Long userId);
 
-    //2 为用户分配角色
-    void doAssign(AssginRoleVo assginRoleVo);
 }
